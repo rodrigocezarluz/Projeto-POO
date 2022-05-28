5853837c602c8c7f19a95410e42b630503b2212f
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-05-28 12:45:59 -0300

    Update LocalizacaoGeografica.h

--- modulo3/LocalizacaoGeografica.h
+++ modulo3/LocalizacaoGeografica.h
@@ -1,1 +1,20 @@
+#ifndef LOCALIZACAOGEOGRAFICA_H
+#define LOCALIZACAOGEOGRAFICA_H
 
+class LocalizacaoGeografica {
+private:
+    double latitude, longitude;
+
+public:
+    LocalizacaoGeografica();
+
+    double getLatitude() const;
+
+    void setLatitude(const double &latitude);
+
+    double getLongitude() const;
+
+    void setLongitude(const double &longitude);
+
+};
+#endif
