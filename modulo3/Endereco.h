f97e7be2b68457f0446b1d7539e7c5107a86c8cd
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-05-28 12:45:28 -0300

    Update Endereco.h

--- modulo3/Endereco.h
+++ modulo3/Endereco.h
@@ -1,1 +1,43 @@
+#ifndef ENDERECO_H
+#define ENDERECO_H
 
+#include <string>
+
+class Endereco {
+private:
+    std::string logradouro, bairro, complemento, cep, cidade, estado;
+    int numero;
+
+public:
+    Endereco();
+
+    int getNumero() const;
+
+    void setNumero(const int &numero);
+
+    std::string getLogradouro() const;
+
+    void setLogradouro(const std::string &logradouro);
+
+    std::string getBairro() const;
+
+    void setBairro(const std::string &bairro);
+
+    std::string getComplemento() const;
+
+    void setComplemento(const std::string &complemento);
+
+    std::string getCep() const;
+
+    void setCep(const std::string &cep);
+
+    std::string getCidade() const;
+
+    void setCidade(const std::string &cidade);
+
+    std::string getEstado() const;
+
+    void setEstado(const std::string &estado);
+
+};
+#endif
