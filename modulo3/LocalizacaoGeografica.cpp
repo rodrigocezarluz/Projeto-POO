bb32f0618bb03bb6db8249903006f23476966e9d
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-05-28 12:45:44 -0300

    Update LocalizacaoGeografica.cpp

--- modulo3/LocalizacaoGeografica.cpp
+++ modulo3/LocalizacaoGeografica.cpp
@@ -1,1 +1,20 @@
+#include "LocalizacaoGeografica.h"
+#include <iostream>
 
+LocalizacaoGeografica::LocalizacaoGeografica() {}
+
+double LocalizacaoGeografica::getLatitude() const {
+    return latitude;
+}
+
+void LocalizacaoGeografica::setLatitude(const double &latitude) {
+    this->latitude = latitude;
+}
+
+double LocalizacaoGeografica::getLongitude() const {
+    return longitude;
+}
+
+void LocalizacaoGeografica::setLongitude(const double &longitude) {
+    this->longitude = longitude;
+}
