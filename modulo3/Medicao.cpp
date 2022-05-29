2f1dc98252bf3f0503726de6a95053c69794ca4c
rmcavalcante7 <rafaelcavalcante7@msn.com>
2022-05-29 16:26:17 -0300

    Implementação da classe de medição do consumo da fatura e de seu método Executar.

--- modulo3/Medicao.cpp
+++ modulo3/Medicao.cpp
@@ -1,1 +1,26 @@
 #include "Medicao.h"
+#include "Fatura.h"
+
+
+
+Medicao::Medicao() : Servico(){}
+
+void Medicao::executar(){
+    // Verificando última fatura da UC
+    double consumoHistorico_inicial = this->getUC().getConsumoEnergiaTotal();
+    double consumoHistorico_final = this->getUC().medirEnergia();
+    double consumoAtualFatura = consumoHistorico_final - consumoHistorico_inicial;
+    
+    Data data;        
+    Data dt_emissao = data.dateNow();
+
+    // todo tem que criar um método na classe Data para adicionar um número de dias a uma data e ter de retorno a nova data
+    Data dt_vencimento(data.dateNow());
+
+    Fatura fatura;
+    fatura.setConsumoEnergia(consumoAtualFatura);
+    fatura.setValorInicial(consumoAtualFatura*Fatura::PRECO_ENERGIA);
+    this->getUC().addFatura(fatura);
+
+
+}
--- modulo3/Medicao.h
+++ modulo3/Medicao.h
@@ -2,9 +2,28 @@
 #define MEDICAO_H
 
 #include "Servico.h"
+#include "Data.h"
+
 
 class Medicao : public Servico {
-    using Servico::Servico;
+    //using Servico::Servico;
+
+
+
+    public:
+        Medicao();
+
+        /**
+         * @brief Executa o serviço de medição de consumo. Toda vez que o serviço é executado é gerada uma nova fatura para a UC
+         * na qual o serviço foi realizado.
+         */
+        void executar() override;
+
+
+
 };
+
+
+ 
 
 #endif
