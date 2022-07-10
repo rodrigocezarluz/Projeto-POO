594914f5d1bfe07dde47162140b643a9537268b5
rafakesia <98476709+rafakesia@users.noreply.github.com>
2022-07-10 16:44:30 -0300

    Update Leiturista.cpp

--- modulo7/Leiturista.cpp
+++ modulo7/Leiturista.cpp
@@ -3,10 +3,11 @@
 //
 
 #include "Leiturista.h"
+#include "Erro.h"
 
 void Leiturista::verificarServico(Servico& servico) {
     if (servico.getTipoServico() != TipoServico::MEDICAO)
-        throw std::invalid_argument("O Funcionario nao esta apto para este tipo de servico.");
+        throw Erro("O Funcionario nao esta apto para este tipo de servico.");
 }
 
 void Leiturista::adicionarServico(Servico &servico, Data data, int prioridade_servico) {
